7907104d975fea8800595bcece552e951057f142
Peter Levinsky <pele@zealand.dk>
2024-09-10 09:25:31 +0200

    Add project files.

---
+++ CharacterSetApp/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Velkommen til tegn Sæt");
+Console.WriteLine();
+
+
+
+for (int i = 32; i < 255; i++)
+{
+    if (i % 10 == 0) Console.WriteLine();
+    Console.Write($"{i:000}={(char)i}  ");
+}
+Console.WriteLine();
+
+
+
+Console.WriteLine(); 
+Console.Write("Press any key");
+Console.ReadKey();
